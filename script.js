14f66970e9e1d0291ef6a45fcef091163e510c1e
Jonathan Ajibosin <jonathanajibosin@Joes-MacBook-Pro.local>
2026-04-13 12:46:16 +0100

    Add products, more products, foodstuff image, about us and request supply sections

--- script.js
+++ script.js
@@ -30,34 +30,3 @@ document.addEventListener("click", (event) => {
     closeMenu();
   }
 });
-
-
-const track = document.querySelector('.slider-track');
-const slides = document.querySelectorAll('.slide');
-
-// 1. Clone the first slide and add it to the end of the track
-const firstClone = slides[0].cloneNode(true);
-track.appendChild(firstClone);
-
-let index = 0;
-const slideWidth = 100; // 100%
-
-function startSliding() {
-  index++;
-  
-  // Add the smooth transition back in (we might have turned it off)
-  track.style.transition = "transform 0.8s cubic-bezier(0.45, 0, 0.55, 1)";
-  track.style.transform = `translateX(-${index * slideWidth}%)`;
-
-  // Check if we are on the clone
-  if (index >= slides.length) {
-    // Wait for the animation to finish before jumping
-    setTimeout(() => {
-      track.style.transition = "none"; // Turn off animation
-      index = 0; // Reset index
-      track.style.transform = `translateX(0)`; // Jump to real first slide
-    }, 800); // This matches the 0.8s transition time
-  }
-}
-
-setInterval(startSliding, 3000);
